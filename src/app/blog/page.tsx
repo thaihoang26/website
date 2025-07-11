8581b1e7c189923b89008f48463ac297c3357d74
thaihoang26 <electron16s19@gmail.com>
2025-07-11 21:51:07 +0700

    Fix UI

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -13,9 +13,13 @@ export default async function BlogPage() {
         {(await posts).map((post) => (
           <li key={post.slug}>
             <Link href={`/blog/${post.slug}`} className="block group">
-              <h2 className="flex items-center text-2xl font-semibold group-hover:text-blue-600 transition">
-                #{post.id} {post.title}
-                <ArrowRight className="ml-2 w-5 h-5 group-hover:translate-x-1 transition-transform" />
+              <h2 className="text-2xl font-semibold group-hover:text-blue-600 transition">
+                <span className="flex items-center justify-between w-full">
+                  <span className="truncate">
+                    #{post.id} {post.title}
+                  </span>
+                  <ArrowRight className="ml-2 w-5 h-5 shrink-0 group-hover:translate-x-1 transition-transform" />
+                </span>
               </h2>
               <p className="text-sm text-gray-500">
                 {format(new Date(post.date), 'HH:mm dd/MM/yyyy')}
